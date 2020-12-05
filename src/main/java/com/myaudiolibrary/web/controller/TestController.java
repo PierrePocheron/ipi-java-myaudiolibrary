1bc005a446622c01cc7b05e647e855bd87406be6
PierrePocheron <pierre.pocheron@gmail.com>
2020-12-05 12:35:02 +0100

    Création route + controller test Installation des differents fichiers de base

---
+++ src/main/java/com/myaudiolibrary/web/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.myaudiolibrary.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class TestController {
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String test(final ModelMap m)
+    {
+        m.put("title", "This is Title Test");
+        m.put("n", "Pierre,");
+        m.put("msg", "How are <strong>you</strong>");
+        m.put("paragraphe", "Bienvenue sur l'application web java Thymeleaf");
+
+        return "test";
+    }
+}
