844afeeaa9df4db8a944d185935df3928bcc533b
PierrePocheron <pierre.pocheron@gmail.com>
2020-12-28 18:07:22 +0100

    Controller : add album OK Template : add album OK

--- src/main/java/com/myaudiolibrary/web/controller/AlbumController.java
+++ src/main/java/com/myaudiolibrary/web/controller/AlbumController.java
@@ -1,4 +1,32 @@
 package com.myaudiolibrary.web.controller;
 
+import com.myaudiolibrary.web.model.Album;
+import com.myaudiolibrary.web.repository.AlbumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+@RequestMapping("/albums")
 public class AlbumController {
+
+    @Autowired
+    private AlbumRepository albumRepository;
+
+
+    @RequestMapping
+            (method = RequestMethod.POST, value = "", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public RedirectView newAlbum(final ModelMap model, Album album)
+    {
+        //VERIFICATIONSS 404 etc
+
+        albumRepository.save(album);
+        model.put("artist",album.getArtist());
+        return new RedirectView("/artists/"+album.getArtist().getArtistId());
+    }
+
 }
