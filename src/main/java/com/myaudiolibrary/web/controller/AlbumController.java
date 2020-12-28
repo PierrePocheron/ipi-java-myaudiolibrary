17bf478675c6f171616cd36a6ff229b5d23609c1
PierrePocheron <pierre.pocheron@gmail.com>
2020-12-28 19:19:49 +0100

    Controller : delete album OK Template : delete album OK

--- src/main/java/com/myaudiolibrary/web/controller/AlbumController.java
+++ src/main/java/com/myaudiolibrary/web/controller/AlbumController.java
@@ -1,14 +1,18 @@
 package com.myaudiolibrary.web.controller;
 
 import com.myaudiolibrary.web.model.Album;
+import com.myaudiolibrary.web.model.Artist;
 import com.myaudiolibrary.web.repository.AlbumRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/albums")
@@ -18,15 +22,22 @@ public class AlbumController {
     private AlbumRepository albumRepository;
 
 
-    @RequestMapping
-            (method = RequestMethod.POST, value = "", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @RequestMapping(method = RequestMethod.POST, value = "", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public RedirectView newAlbum(final ModelMap model, Album album)
     {
         //VERIFICATIONSS 404 etc
-
         albumRepository.save(album);
         model.put("artist",album.getArtist());
         return new RedirectView("/artists/"+album.getArtist().getArtistId());
+    }
+
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{albumId}")
+    public RedirectView deleteAlbum(@PathVariable("albumId") Long albumId, final ModelMap modelMap, Artist artist){
+        System.out.println("Suppression de l album : " + albumId);
+        albumRepository.deleteById(albumId);
+
+        return new RedirectView("/artists/"+artist.getArtistId());
     }
 
 }
