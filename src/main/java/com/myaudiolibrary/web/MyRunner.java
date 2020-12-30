88f8b40c1c886ef1f17d480f7618b17bb7f13183
PierrePocheron <pierre.pocheron@gmail.com>
2020-12-30 22:36:14 +0100

    Ajout systeme de template : fragments

--- src/main/java/com/myaudiolibrary/web/MyRunner.java
+++ src/main/java/com/myaudiolibrary/web/MyRunner.java
@@ -24,31 +24,13 @@ public class MyRunner implements CommandLineRunner {
     public void run(String... args) throws Exception {
         System.out.println("================================================================================\n");
         System.out.println("Salut les devs, Bienvenue dans mon Application Web Java ! ");
-        System.out.println("Le Server a bien démarrer correctement ");
-        System.out.println("Accessible à : http://localhost:5366");
+        System.out.println("Le serveur a bien démarré correctement ");
+        System.out.println("Accueil : http://localhost:5366");
         System.out.println("Page de test : http://localhost:5366/test");
         System.out.println("Detail d'un artist : http://localhost:5366/artists/1");
         System.out.println("Liste des artists : http://localhost:5366/artists?page=0&size=10&sortProperty=name&sortDirection=ASC");
         System.out.println("Création d'un artist : http://localhost:5366/artists/new\n");
         System.out.println("================================================================================\n");
-
-//        System.out.println("====================");
-//        Artist ar1 = artistRepository.findByArtistId(10L);
-//        System.out.println(ar1.toString());
-//        System.out.println("====================\n");
-
-//        System.out.println("====================");
-//        List<Album> albumList = albumRepository.findAll();
-//        for(Album album : albumList){
-//            System.out.println(album.toString());
-//        }
-//        System.out.println("====================\n");
-
-//        Optional<Album> optionalAlbum = albumRepository.findById(10L);
-//        System.out.println(optionalAlbum.toString());
-
-//        Artist artist = artistRepository.findByArtistId(1L);
-//        System.out.println(artist.toString());
     }
     public static void print(Object t) {
         System.out.println(t);
